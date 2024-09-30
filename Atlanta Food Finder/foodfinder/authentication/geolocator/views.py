39981290d277beeec1504f0bf226afb4930cb54c
anushak016 <156885478+anushak016@users.noreply.github.com>
2024-09-29 21:17:09 -0400

    Delete Atlanta Food Finder/foodfinder/authentication/geolocator directory

--- Atlanta Food Finder/foodfinder/authentication/geolocator/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-from atlantaFoodFinder import settings
-from .models import Restaurant
-
-def geolocator(request):
-    restaurants = Restaurant.objects.all()
-    return render(request, 'geolocator/map.html', {'restaurants': restaurants,
-                                                   'google_api_key': settings.GOOGLE_API_KEY,})
